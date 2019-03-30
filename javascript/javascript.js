b7a735b78f190f7bce4856e3d6d1075ac8e7606a
Blake Feldman <blake.feldman@gmail.com>
2019-03-30 11:52:08 -0500

    merge changes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,3 +7,33 @@ var config = {
     messagingSenderId: "388883978030"
   };
   firebase.initializeApp(config);
+
+var db = firebase.database();
+var formRef = db.ref("/message");
+
+$("#submit-form").click(function (event) {
+event.preventDefault()
+   var name = $("#employee-name").val();
+   var role = $("#role").val();
+   var startYear = $("#start-year").val();
+   var rate = $("#monthly-rate").val();
+   formRef.push({
+       name: name,
+       role: role,
+       startYear: startYear,
+       rate: rate
+   });
+   console.log(name);
+   console.log(role);
+   console.log(startYear);
+   console.log(rate);
+})
+
+
+
+// messageRef.on('child_added', function (data) {
+//    // addCommentElement(postElement, data.key, data.val().text, data.val().author);
+//    var message = data.val().text;
+//    var messageElement = $("<p>").text(message);
+//    $("#message-container").append(messageElement);
+// });
